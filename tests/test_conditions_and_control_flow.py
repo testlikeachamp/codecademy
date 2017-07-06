d11106502c364f9a71605a7ed21723e57f470fc0
Alexander Onishchenko <a.g.onishchenko@gmail.com>
2017-07-06 11:46:40 +0100

    refactoring unit test using param fixuters and func

--- tests/test_conditions_and_control_flow.py
+++ tests/test_conditions_and_control_flow.py
@@ -1,5 +1,19 @@
 from codecademy.conditionals_and_control_flow import greater_less_equal_5
 
+import sys, pytest
 
-def test_greater_less_equal_5():
-    assert greater_less_equal_5(6) == 1
+@pytest.mark.parametrize("test_input,expected", [
+    (sys.maxsize-1, 1),
+    (100-1, 1),
+    (100, 1),
+    (6, 1),
+    (5, 0),
+    (4, -1),
+    (2, -1),
+    (0, -1),
+    (-1, -1),
+    (-100+1, -1),
+    (-sys.maxsize+1, -1),
+])
+def test_greater_less_equal_5l(test_input, expected):
+    assert greater_less_equal_5(test_input) == expected
