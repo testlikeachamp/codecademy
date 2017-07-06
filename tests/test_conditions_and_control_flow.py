db5a36c346ae352cfa49c73d4c7163ca68457d4b
Alexander Onishchenko <a.g.onishchenko@gmail.com>
2017-07-06 09:11:02 +0100

    add unit test to greater_less_equal_5 function

--- tests/test_conditions_and_control_flow.py
+++ tests/test_conditions_and_control_flow.py
@@ -1,5 +1,38 @@
 from codecademy.conditionals_and_control_flow import greater_less_equal_5
 
+import sys
 
-def test_greater_less_equal_5():
+def test_max_greater_5():
+    assert greater_less_equal_5(sys.maxint-1) == 1
+
+def test_100_greater_5():
+    assert greater_less_equal_5(100-1) == 1
+
+def test_100_minus_1_greater_5():
+    assert greater_less_equal_5(100) == 1
+
+def test_greater_5():
     assert greater_less_equal_5(6) == 1
+
+def test_equal_5():
+    assert greater_less_equal_5(5) == 0
+
+def test_less_5():
+    assert greater_less_equal_5(4) == -1
+
+def test_2_less_5():
+    assert greater_less_equal_5(2) == -1
+
+def test_0_less_5():
+    assert greater_less_equal_5(0) == -1
+
+def test_minus_1_less_5():
+    assert greater_less_equal_5(-1) == -1
+
+def test_minus_100_plus_1_less_5():
+    assert greater_less_equal_5(-100+1) == -1
+
+def test_min_plus_1_less_5():
+    assert greater_less_equal_5(-sys.maxint+1) == -1
+
+
