4a72b543ca6cc76d9d036c488e379e3c397ca190
Alexander Onishchenko <a.g.onishchenko@gmail.com>
2017-07-07 09:40:55 +0100

    sort tuples by first value in ascending order

--- tests/test_conditions_and_control_flow.py
+++ tests/test_conditions_and_control_flow.py
@@ -3,17 +3,17 @@ from codecademy.conditionals_and_control_flow import greater_less_equal_5
 import sys, pytest
 
 @pytest.mark.parametrize("test_input,expected", [
-    (sys.maxsize-1, 1),
-    (100-1, 1),
-    (100, 1),
-    (6, 1),
-    (5, 0),
-    (4, -1),
-    (2, -1),
-    (0, -1),
-    (-1, -1),
-    (-100+1, -1),
     (-sys.maxsize+1, -1),
+    (-100+1, -1),
+    (-1, -1),
+    (0, -1),
+    (2, -1),
+    (4, -1),
+    (5, 0),
+    (6, 1),
+    (100, 1),
+    (100-1, 1),
+    (sys.maxsize-1, 1),
 ])
 def test_greater_less_equal_5l(test_input, expected):
     assert greater_less_equal_5(test_input) == expected
