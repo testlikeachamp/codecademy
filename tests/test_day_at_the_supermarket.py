ef349a1d0064d157eac4d4c4ab93a0959c2715a2
Alexander Onishchenko <a.g.onishchenko@gmail.com>
2017-07-24 08:22:39 +0100

    add assert fail and rename test parametres

--- tests/test_day_at_the_supermarket.py
+++ tests/test_day_at_the_supermarket.py
@@ -5,23 +5,23 @@ import pytest
 from codecademy.day_at_the_supermarket import compute_bill
 
 
-@pytest.mark.parametrize("test_compute_bill_input,test_compute_bill_expected", [
+@pytest.mark.parametrize("input,expected", [
     (["banana"], 4),
     (["banana", "orange", "pear"], 8.5),
     (["banana", "orange", "pear", "apple"], 8.5),
     (["banana", "orange", "orange", "pear", "apple"], 10),
     (["banana", "banana", "orange", "pear", "apple"], 12.5)
 ])
-def test_compute_bill(test_compute_bill_input, test_compute_bill_expected):
-    assert compute_bill(test_compute_bill_input) == test_compute_bill_expected
+def test_compute_bill(input, expected):
+    assert compute_bill(input) == expected
 
 
-@pytest.mark.parametrize("test_compute_bil_negative_input", [[], [0, 1, 3, False, None], ["orange", "pear"],
-                                                             ["orange", "pear", "potato"],
+@pytest.mark.parametrize("input", [[], [0, 1, 3, False, None], ["orange", "pear"], ["orange", "pear", "potato"],
                                                              ["banana", "banana", "orange", "pear", "apple"],
                                                              0, False, True, None])
-def test_compute_bill_negative(test_compute_bil_negative_input):
+def test_compute_bill_negative(input):
     try:
-        compute_bill(test_compute_bil_negative_input)
+        compute_bill(input)
+        assert str(False) + " error enter type"
     except AssertionError as e:
-        assert str(e) == str(test_compute_bil_negative_input) + " error enter type"
+        assert str(e) == str(input) + " error enter type"
