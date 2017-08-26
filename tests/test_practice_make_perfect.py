b7ce5934fcc9a282b92b0be80bb85e102ac6eb0c
nicolas13sochi <nikolay.grishchenko@gmail.com>
2017-08-26 03:56:39 +0300

    Added my part of practice makes perfect (functions and tests)

--- tests/test_practice_make_perfect.py
+++ tests/test_practice_make_perfect.py
@@ -4,6 +4,7 @@ import pytest
 
 
 from codecademy.practice_make_perfect import count, factorial, is_int, median, product, reverse, scrabble_score
+from codecademy.practice_make_perfect import is_even, digit_sum, is_prime, anti_vowel, censor, purify, remove_duplicates
 
 
 @pytest.mark.parametrize("input_val,expected", [
@@ -150,7 +151,9 @@ def test_scrabble_score_negative(input_neg):
     ({"some_key": 123}, 1),
 ])
 def test_count(input_val2, expected):
-    assert count(["a", False, True, False, -1, 0, 1, [1, 2, 3], ("test", 123), {"some_key": 123}], input_val2) == expected
+    assert count(["a", False, True, False, -1, 0, 1, [1, 2, 3],
+                  ("test", 123), {"some_key": 123}], input_val2) == expected
+
 # END count
 
 
@@ -215,3 +218,229 @@ def test_median_negative(input_neg):
     except AssertionError as e:
         assert str(e) == "Passed the wrong data type"
 
+
+@pytest.mark.parametrize("test_input,expected", [
+    (-sys.maxsize-2, False),
+    (-sys.maxsize-1,  True),
+    (-sys.maxsize,  False),
+    (-3037000499, False),
+    (-3037000498, True),
+    (-10, True),
+    (-9, False),
+    (-8, True),
+    (-1, False),
+    (0, True),
+    (1, False),
+    (8, True),
+    (9, False),
+    (10, True),
+    (3037000498, True),
+    (3037000499, False),
+    (sys.maxsize-1, True),
+    (sys.maxsize, False),
+    (sys.maxsize+1, True),
+])
+def test_is_even(test_input, expected):
+    assert is_even(test_input) == expected
+
+
+@pytest.mark.parametrize("input_negative", [
+    None,
+    True,
+    False,
+    0.1,
+    -1.1,
+    "some text",
+    ["a"],
+    [False, True, 1, 2, "some text"]
+])
+def test_is_even_negative(input_negative):
+    try:
+        is_even(input_negative)
+        raise Exception("Expected an exception from is_even()")
+    except AssertionError as e:
+        assert str(e) == "{} is not integer".format(input_negative)
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    (1, 1),
+    (12, 3),
+    (123, 6),
+    (1234, 10),
+    (12345, 15),
+    (123456, 21),
+    (sys.maxsize-1, 87),
+    (sys.maxsize, 88),
+    (sys.maxsize+1, 89),
+])
+def test_digit_sum(test_input, expected):
+    assert digit_sum(test_input) == expected
+
+
+@pytest.mark.parametrize("input_negative", [
+    None,
+    True,
+    False,
+    -sys.maxsize,
+    -1.1,
+    0,
+    0.1,
+    1.1,
+    "some text",
+    ["a"],
+    [False, True, 1, 2, "some text"]
+])
+def test_digit_sum_negative(input_negative):
+    try:
+        digit_sum(input_negative)
+        raise Exception("Expected an exception from digit_num()")
+    except AssertionError as e:
+        assert str(e) == "{} is not integer or not a positive integer".format(input_negative)
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    (-sys.maxsize - 2, False),
+    (-sys.maxsize - 1, False),
+    (-sys.maxsize, False),
+    (-3037000499, False),
+    (-10, False),
+    (-3, False),
+    (-2, False),
+    (-1, False),
+    (0, False),
+    (1, False),
+    (2, True),
+    (3, True),
+    (4, False),
+    (5, True),
+    (13, True),
+    (15, False),
+    (227, True),
+    (sys.maxsize - 1, False),
+    (sys.maxsize, False),
+    (sys.maxsize + 1, False),
+])
+def test_is_prime(test_input, expected):
+    assert is_prime(test_input) == expected
+
+
+@pytest.mark.parametrize("input_negative", [
+    None,
+    True,
+    False,
+    -1.1,
+    0.1,
+    1.1,
+    "some text",
+    ["a"],
+    [False, True, 1, 2, "some text"]
+])
+def test_is_prime_negative(input_negative):
+    try:
+        is_prime(input_negative)
+        raise Exception("Expected an exception from is_prime()")
+    except AssertionError as e:
+        assert str(e) == "{} is not integer".format(input_negative)
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ('This is test and tasty', 'Ths s tst nd tsty'),
+    ('AaOoEeIiQqUu', ''),
+    ('AaOoEeIiQqUuYy', 'Yy'),
+    (3037000499, '3037000499'),
+    (False, 'Fls'),
+    (True, 'Tr'),
+    (None, 'Nn'),
+    ([1, 2, 'O'], '[1, 2, \'\']'),
+    ({'Yes': 'The right answer'}, '{\'Ys\': \'Th rght nswr\'}'),
+    (sys.maxsize, '9223372036854775807'),
+])
+def test_anti_vowel(test_input, expected):
+    assert anti_vowel(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input_text,test_input_word,expected", [
+    ('This is test and that is tasty', 'is', 'This ** test and that ** tasty'),
+    ('Aa Oo Ee Ii Qq Uu', 'Aa Oo', 'Aa Oo Ee Ii Qq Uu'),
+    ('the THE the THe', 'the', '*** THE *** THe'),
+    ('We are made of steel', 'They', 'We are made of steel')
+])
+def test_censor(test_input_text, test_input_word, expected):
+    assert censor(test_input_text, test_input_word) == expected
+
+
+@pytest.mark.parametrize("text_input_negative, word_input_negative", [
+    (3037000499, '3037000499'),
+    (False, 'Fls'),
+    (True, 'Tr'),
+    (None, 'Nn'),
+    ([1, 2, 'O'], '[1, 2, \'\']'),
+    ({'Yes': 'The right answer'}, '{\'Ys\': \'Th rght nswr\'}'),
+    (sys.maxsize, '9223372036854775807'),
+])
+def test_censor_negative(text_input_negative, word_input_negative):
+    try:
+        censor(text_input_negative, word_input_negative)
+        raise Exception("Expected an exception from is_prime()")
+    except AssertionError as e:
+        assert str(e) == "Passed the wrong data type"
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ([0, 1, 2, 3, 4], [0, 2, 4]),
+    ([-4, -3, -2, -1, 0], [-4, -2, 0]),
+    ([1, 3, 5], []),
+])
+def test_purify(test_input, expected):
+    assert purify(test_input) == expected
+
+
+@pytest.mark.parametrize("input_negative", [
+    None,
+    True,
+    False,
+    -1.1,
+    0.1,
+    1.1,
+    "some text",
+    ["a"],
+    [False, True, 1, 2, "some text"],
+    [1, 3, 4, 5, "a"]
+])
+def test_purify_negative(input_negative):
+    try:
+        purify(input_negative)
+        raise Exception("Expected an exception from is_prime()")
+    except AssertionError as e:
+        assert str(e) == "Passed the wrong data type" or\
+               "Passed the wrong data type - wrong element inside list"
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ([0, 1, 2, 3, 4, 3, 2, 1], [0, 4, 3, 2, 1]),
+    ([-4, -3, -2, -1, 0, -1, -1], [-4, -3, -2, 0, -1]),
+    ([1, 1, 1, 1], [1]),
+])
+def test_remove_duplicates(test_input, expected):
+    assert remove_duplicates(test_input) == expected
+
+
+@pytest.mark.parametrize("input_negative", [
+    None,
+    True,
+    False,
+    -1.1,
+    0.1,
+    1.1,
+    "some text",
+    ["a"],
+    [False, True, 1, 2, "some text"],
+    [1, 3, 4, 5, "a"]
+])
+def test_remove_duplicates_negative(input_negative):
+    try:
+        remove_duplicates(input_negative)
+        raise Exception("Expected an exception from is_prime()")
+    except AssertionError as e:
+        assert str(e) == "Passed the wrong data type" or\
+               "Passed the wrong data type - wrong element inside list"
