76e1c7823c8bbb762497229e34307f3845275b45
Artem Selekhov <a-selehov@yandex.ru>
2017-08-27 22:34:15 +0300

    Corrected one boundary value. Removed unnecessary comments
    Signed-off-by: Artem Selekhov <a-selehov@yandex.ru>

--- tests/test_wether_report.py
+++ tests/test_wether_report.py
@@ -2,27 +2,7 @@ import pytest
 
 from codecademy.functions_artem import weather_report
 
-#
-# def test_greater_less_equal_5_minus_1():
-#     assert greater_less_equal_5(-1) == -1
-#
-#
-# def test_greater_less_equal_5_6():
-#     assert greater_less_equal_5(6) == 1
-#
-#
-# def test_greater_less_equal_5_5():
-#     assert greater_less_equal_5(5) == 0
-
-"""def weather_report(temperature):
-    if temperature < 0:
-        return "freezing"
-    elif 0 <= temperature < 10:
-        return 'cold'
-    else:
-        return 'warm'"""
-# -&     -10000000 ..... -1  0  +1 ...  9  ..10..12.... 100000 ..... +&
-
+# -&     -10000000 ..... -1  0  +1 ...  9  ..10..11.... 100000 ..... +&
 
 @pytest.mark.parametrize("temperature,expected", [
     (-10000000, "freezing"),
@@ -31,7 +11,7 @@ from codecademy.functions_artem import weather_report
     (1, "cold"),
     (9, "cold"),
     (10, "warm"),
-    (12, "warm"),
+    (11, "warm"),
     (100000, "warm")
 ])
 def test_weather_report(temperature, expected):
