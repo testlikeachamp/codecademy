e548b95e108911f5b416a95200045c709a48d641
nicolas13sochi <nikolay.grishchenko@gmail.com>
2017-08-27 11:31:42 +0300

    Added my part of exercises at exam statistics (#27)

--- codecademy/exam_statistics.py
+++ codecademy/exam_statistics.py
@@ -2,7 +2,7 @@ def grades_sum(scores):
     assert isinstance(scores, list), "Passed the wrong data type"
     j = 0
     for i in scores:
-        assert type(i) == int and i >= 0, "Passed the wrong data type"
+        assert type(i) in (int, float) and i >= 0, "Passed the wrong data type"
         j += i
     return j
 
@@ -16,9 +16,44 @@ def grades_average(scores):
 def grades_variance(scores):
     average = grades_average(scores)
     variance = 0
-    a = 0
     for score in scores:
         variance += (average - score) ** 2
     return round(variance / float(len(scores)), 2)
 
 
+# 4 7 9 nicolas13sochi part
+# the exercises ## 4,7 are duplicated with Sanya's task above
+# 4 The sum of scores
+
+grades = [100, 100, 90, 40, 80, 100, 85, 70, 90, 65, 90, 85, 50.5]
+
+
+def grades_sum_4(scores):
+    assert isinstance(scores, list), "Passed the wrong data type"
+    total = 0
+    for score in scores:
+        assert type(score) in (int, float) and score >= 0, "Passed the wrong data type"
+        total += score
+    return total
+
+
+# 7 The Variance
+
+
+def grades_variance_7(scores):
+    assert isinstance(scores, list), "Passed the wrong data type"
+    average = grades_average(scores)
+    variance = 0
+    for score in scores:
+        assert type(score) in (int, float) and score >= 0, "Passed the wrong data type"
+        variance += (average - score) ** 2
+    return round(variance/len(scores), 2)
+
+
+# 9 Review
+def grades_std_deviation(scores):
+    assert isinstance(scores, list), "Passed the wrong data type"
+    variance = grades_variance(scores)
+    return round(variance ** 0.5, 2)
+
+print(grades_std_deviation(grades))
--- tests/test_exam_statistics.py
+++ tests/test_exam_statistics.py
@@ -1,8 +1,7 @@
-import sys
-
 import pytest
 
 from codecademy.exam_statistics import grades_average, grades_sum, grades_variance
+from codecademy.exam_statistics import grades_sum_4, grades_variance_7, grades_std_deviation
 
 
 @pytest.mark.parametrize("input_val,expected", [
@@ -60,6 +59,68 @@ def test_grades_variance(input_val, expected):
 def test_grades_variance_negative(input_val):
     try:
         grades_variance(input_val)
+        raise Exception("Expected an exception from grades_sum")
+    except AssertionError as e:
+        assert str(e) == "Passed the wrong data type"
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ([0, 1], 1),
+    ([0, 1, 2], 3),
+    ([0, 1, 10, 1], 12),
+    ([0, 10, 20, 30], 60),
+    ([1000, 1, 9, 100], 1110),
+])
+def test_grades_sum_4(test_input, expected):
+    assert grades_sum_4(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input_negative", [-1, "blah", None, False, [0, 1, "test", -1, "blah", None, False]])
+def test_grades_sum_4_negative(test_input_negative):
+    try:
+        grades_sum_4(test_input_negative)
+        raise Exception("Expected an exception from grades_sum")
+    except AssertionError as e:
+        assert str(e) == "Passed the wrong data type"
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ([0], 0),
+    ([0, 1], 0.25),
+    ([0, 1, 2], 0.67),
+    ([0, 1, 1, 2, 1], 0.4),
+    ([10, 10, 10], 0),
+    ([10, 50, 80, 100], 1150.0),
+])
+def test_grades_variance_7(test_input, expected):
+    assert grades_variance_7(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input_negative", [-1, "blah", None, False, [0, 1, "test", -1, "blah", None, False]])
+def test_grades_variance_7_negative(test_input_negative):
+    try:
+        grades_variance_7(test_input_negative)
+        raise Exception("Expected an exception from grades_sum")
+    except AssertionError as e:
+        assert str(e) == "Passed the wrong data type"
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ([0], 0),
+    ([0, 1], 0.5),
+    ([0, 1, 2], 0.82),
+    ([0, 1, 1, 2, 1], 0.63),
+    ([10, 10, 10], 0),
+    ([10, 50, 80, 100], 33.91),
+])
+def test_grades_std_variance(test_input, expected):
+    assert grades_std_deviation(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input_negative", [-1, "blah", None, False, [0, 1, "test", -1, "blah", None, False]])
+def test_grades_std_variance_negative(test_input_negative):
+    try:
+        grades_std_deviation(test_input_negative)
         raise Exception("Expected an exception from grades_sum")
     except AssertionError as e:
         assert str(e) == "Passed the wrong data type"
